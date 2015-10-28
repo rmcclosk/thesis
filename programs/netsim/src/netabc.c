dc1c31c35260fabf0831ad077624b55a4355c5f8
Rosemary McCloskey <rmcclosk.math@gmail.com>
2015-10-28 10:40:08 -0700

    Started adding yaml parser

--- programs/netsim/src/netabc.c
+++ programs/netsim/src/netabc.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <float.h>
 #include <string.h>
+#include <yaml.h>
 #include <igraph/igraph.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_rng.h>
@@ -17,12 +18,15 @@
 
 #define NNODE 5000
 #define NSIMNODE 1000
-#define MEAN_DEGREE 4
+#define BA_MEAN_DEGREE 4
 #define PA_POWER_MIN 0.0
 #define PA_POWER_MAX 1.0
+#define ER_MEAN_DEGREE_MIN 1.2
+#define ER_MEAN_DEGREE_MAX 12
 
 struct netabc_options {
     FILE *tree_file;
+    FILE *yaml_file;
     int nthread;
     int seed;
     int nparticle;
@@ -49,7 +53,7 @@ struct option long_options[] =
 
 void usage(void)
 {
-    fprintf(stderr, "Usage: netabc [options] [tree]\n\n");
+    fprintf(stderr, "Usage: netabc [options] [tree] [yaml_file]\n\n");
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "  -h, --help                display this message\n");
     fprintf(stderr, "  -t, --num-threads         number of threads\n");
@@ -67,6 +71,7 @@ struct netabc_options get_options(int argc, char **argv)
     int i, c = 0;
     struct netabc_options opts = {
         .tree_file = stdin,
+        .yaml_file = NULL,
         .nthread = 1,
         .seed = -1,
         .decay_factor = 0.2,
@@ -126,8 +131,48 @@ struct netabc_options get_options(int argc, char **argv)
     if (optind < argc) {
         opts.tree_file = fopen(argv[optind++], "r");
     }
+    if (optind < argc) {
+        opts.yaml_file = fopen(argv[optind++], "r");
+    }
 
     return opts;
+}
+
+typedef enum {
+    NNODE = 0,
+    NSIMNODE = 1,
+    SIM_TIME = 2,
+    TRANSMIT_RATE = 3,
+    REMOVE_RATE = 4,
+    EDGES = 5,
+    MAX_VALUE = EDGES
+} net_parameter;
+
+void get_parameters(FILE *f, smc_distribution *priors, double *prior_params)
+{
+    int i = 0, pcur = 0;
+    yaml_parser_t parser;
+    yaml_token_t token;
+
+    yaml_parser_set_input_file(&parser, f);
+    do
+    {
+        yaml_parser_scan(&parser, &token);
+        switch (token.type)
+        {
+            case YAML_SCALAR_TOKEN:
+                fprintf(stderr, "%s\n", token.data.scalar.value);
+                break;
+            default:
+                break;
+        }
+        if (token.type != YAML_STREAM_END_TOKEN)
+            yaml_token_delete(&token);
+    }
+    while (token.type != YAML_STREAM_END_TOKEN);
+
+    yaml_token_delete(&token);
+    yaml_parser_delete(&parser);
 }
 
 void ba_propose(gsl_rng *rng, double *theta, const void *params)
@@ -162,7 +207,7 @@ void ba_sample_dataset(gsl_rng *rng, const double *theta, const void *arg, void 
     igraph_rng_set_default(&igraph_rng);
 
     igraph_vector_init(&v, NNODE);
-    igraph_barabasi_game(&net, NNODE, *theta, MEAN_DEGREE/2, NULL, 0,
+    igraph_barabasi_game(&net, NNODE, *theta, BA_MEAN_DEGREE/2, NULL, 0,
             1, 0, IGRAPH_BARABASI_PSUMTREE, NULL);
     igraph_to_directed(&net, IGRAPH_TO_DIRECTED_MUTUAL);
 
@@ -189,53 +234,76 @@ void ba_sample_dataset(gsl_rng *rng, const double *theta, const void *arg, void 
     igraph_destroy(&net);
 }
 
+void er_sample_dataset(gsl_rng *rng, const double *theta, const void *arg, void *X)
+{
+    int i;
+    igraph_t net;
+    igraph_t *tree = (igraph_t *) X;
+    igraph_vector_t v;
+    igraph_rng_t igraph_rng;
+    unsigned long int igraph_seed = gsl_rng_get(rng);
+    int ntip = (int) ((double *) arg)[0];
+    double decay_factor = ((double *) arg)[1];
+    double rbf_variance = ((double *) arg)[2];
+
+    // because of thread-local storage this might work?
+    igraph_rng_init(&igraph_rng, &igraph_rngtype_mt19937);
+    igraph_rng_seed(&igraph_rng, igraph_seed);
+    igraph_rng_set_default(&igraph_rng);
+
+    igraph_vector_init(&v, NNODE);
+
+    igraph_erdos_renyi_game(&net, IGRAPH_ERDOS_RENYI_GNP, NNODE, *theta / NNODE, 0, 0);
+    igraph_to_directed(&net, IGRAPH_TO_DIRECTED_MUTUAL);
+    
+    igraph_vector_fill(&v, 0);
+    SETVANV(&net, "remove", &v);
+    for (i = 0; i < NNODE; ++i) {
+        VECTOR(v)[i] = i;
+    }
+    SETVANV(&net, "id", &v);
+
+    igraph_vector_resize(&v, igraph_ecount(&net));
+    igraph_vector_fill(&v, 1);
+    SETEANV(&net, "transmit", &v);
+    
+    simulate_phylogeny(tree, &net, rng, INFINITY, NSIMNODE, 1);
+    i = 0;
+    while (igraph_vcount(tree) < (ntip - 1) / 2) {
+        if (i == 20) {
+            fprintf(stderr, "Too many tries to simulate a tree\n");
+            tree = NULL;
+            break;
+        }
+        igraph_destroy(tree);
+        simulate_phylogeny(tree, &net, rng, INFINITY, NSIMNODE, 1);
+        ++i;
+    }
+
+    if (tree != NULL) {
+        subsample_tips(tree, ntip, rng);
+        ladderize(tree);
+        scale_branches(tree, MEAN);
+        SETGAN(tree, "kernel", kernel(tree, tree, decay_factor, rbf_variance, 1));
+    }
+
+    igraph_rng_set_default(igraph_rng_default());
+    igraph_rng_destroy(&igraph_rng);
+    igraph_vector_destroy(&v);
+    igraph_destroy(&net);
+}
+
 double ba_distance(const void *x, const void *y, const void *arg)
 {
+    if (x == NULL || y == NULL)
+        return INFINITY;
     igraph_t *gx = (igraph_t *) x;
     igraph_t *gy = (igraph_t *) y;
     double decay_factor = ((double *) arg)[0];                                   
     double rbf_variance = ((double *) arg)[1]; 
-    double nx = NTIP(gx), ny = NTIP(gy);
     double kx = GAN(gx, "kernel");
     double ky = GAN(gy, "kernel");
-    double d[14];
-    double w[14] = {9.33802949226047e-01, 2.18403741961014e+05,
-    1.13819995020497e+01, 7.13025882178519e-02, 5.477396722075e-01,
-    6.00418896265072e-02, 4.41223417569416e+05, 6.51247258113592e+00,
-    1.24508698036331e+01, 9.17636517221493e-01, 6.89583117577029e-04,
-    8.78845237640112e-02, 1, 1};
-    double dist = 0;
-    int i;
-
-    /*
-    d[0] = ((double) colless(gx) - COLLESS_YULE(nx)) / nx -
-           ((double) colless(gy) - COLLESS_YULE(ny)) / ny;
-    d[1] = (double) il_nodes(gx) / nx - (double) il_nodes(gy) / ny;
-    d[2] = (double) ladder_length(gx) / nx - (double) ladder_length(gy) / ny;
-    d[3] = (double) width(gx) / (double) ladder_length(gx) - 
-           (double) width(gy) / (double) ladder_length(gy);
-    d[4] = (double) max_delta_width(gx) / nx - (double) max_delta_width(gy) / ny;
-    d[5] = (double) width(gx) / nx - (double) width(gy) / ny;
-    d[6] = (double) cherries(gx) / nx - (double) cherries(gy) / ny;
-    d[7] = prop_unbalanced(gx) - prop_unbalanced(gy);
-    d[8] = avg_unbalance(gx) - avg_unbalance(gy);
-    d[9] = (double) (sackin(gx, 0) - SACKIN_YULE(nx)) / nx -
-           (double) (sackin(gy, 0) - SACKIN_YULE(ny)) / ny;
-    d[10] = (double) (cophenetic(gx, 0) - COPHENETIC_YULE(nx)) / nx -
-            (double) (cophenetic(gy, 0) - COPHENETIC_YULE(ny)) / ny;
-    d[11] = pybus_gamma(gx) - pybus_gamma(gy);
-    */
-    d[12] = 1.0 - kernel(gx, gy, decay_factor, rbf_variance, 1) / sqrt(kx) / sqrt(ky);
-    //d[13] = 1.0 - nLTT(gx, gy);
-
-    /*
-    for (i = 0; i < 14; ++i) {
-        dist += pow(w[i] * d[i], 2);
-    }
-    */
-    //dist = pow(d[2], 2) + pow(d[8], 2);
-    //return sqrt(dist);
-    return d[12];
+    return 1.0 - kernel(gx, gy, decay_factor, rbf_variance, 1) / sqrt(kx) / sqrt(ky);
 }
 
 void ba_feedback(const double *theta, int nparticle, void *params)
@@ -258,7 +326,16 @@ smc_functions ba_functions = {
     .destroy_dataset = ba_destroy_dataset
 };
 
-smc_config ba_config = {
+smc_functions er_functions = {
+    .propose = ba_propose,
+    .proposal_density = ba_proposal_density,
+    .sample_dataset = er_sample_dataset,
+    .distance = ba_distance,
+    .feedback = ba_feedback,
+    .destroy_dataset = ba_destroy_dataset
+};
+
+smc_config config = {
     .nparam = 1,
     .final_epsilon = 0.005,
     .step_tolerance = 1e-5,
@@ -295,29 +372,29 @@ int main (int argc, char **argv)
     ntip = (igraph_vcount(tree) + 1) / 2;
     
     // set up SMC configuration
-    ba_config.nparticle = opts.nparticle;
-    ba_config.ess_tolerance = opts.nparticle / 2;
-    ba_config.nsample = opts.nsample;
-    ba_config.quality = opts.quality;
+    config.nparticle = opts.nparticle;
+    config.ess_tolerance = opts.nparticle / 2;
+    config.nsample = opts.nsample;
+    config.quality = opts.quality;
 
-    ba_config.priors = malloc(sizeof(smc_distribution));
-    ba_config.priors[0] = UNIFORM;
-    ba_config.prior_params = malloc(2 * sizeof(double));
-    ba_config.prior_params[0] = PA_POWER_MIN;
-    ba_config.prior_params[1] = PA_POWER_MAX;
+    config.priors = malloc(sizeof(smc_distribution));
+    config.priors[0] = UNIFORM;
+    config.prior_params = malloc(2 * sizeof(double));
+    config.prior_params[0] = ER_MEAN_DEGREE_MIN;
+    config.prior_params[1] = ER_MEAN_DEGREE_MAX;
 
     sample_dataset_arg[0] = ntip;
     sample_dataset_arg[1] = opts.decay_factor;
     sample_dataset_arg[2] = opts.rbf_variance;
-    ba_config.sample_dataset_arg = &sample_dataset_arg;
+    config.sample_dataset_arg = &sample_dataset_arg;
 
     distance_arg[0] = opts.decay_factor;
     distance_arg[1] = opts.rbf_variance;
     distance_arg[2] = opts.use_nltt;
-    ba_config.distance_arg = &distance_arg;
+    config.distance_arg = &distance_arg;
 
-    ba_result = abc_smc(ba_config, ba_functions, opts.seed, opts.nthread, tree);
-    for (i = 0; i < ba_config.nparticle; ++i)
+    ba_result = abc_smc(config, er_functions, opts.seed, opts.nthread, tree);
+    for (i = 0; i < config.nparticle; ++i)
         printf("%f\n", ba_result->theta[i]);
 
     igraph_destroy(tree);
@@ -325,7 +402,10 @@ int main (int argc, char **argv)
     if (opts.tree_file != stdin) {
         fclose(opts.tree_file);
     }
-    free(ba_config.priors);
-    free(ba_config.prior_params);
+    if (opts.yaml_file != NULL) {
+        fclose(opts.yaml_file);
+    }
+    free(config.priors);
+    free(config.prior_params);
     return EXIT_SUCCESS;
 }
