0c6cdbfd9260e702a6d78009cc3b9856157dd449
Rosemary <rmcclosk.math@gmail.com>
2016-05-24 12:22:49 -0700

    Added function to compute weighted HPD

---
+++ programs/netabc/R/hpd.R
@@ -0,0 +1,14 @@
+#' Calculated a weighted highest density interval.
+#'
+#' @param x sample from distribution
+#' @param wt weights of each element in the sample
+#' @param conf proportion of probability density contained in interval
+#' @return a two-element vector containing upper and lower HPD interval bounds
+#' @export
+wtd.hpd <- function (x, wt, conf=0.95) {
+    cdf <- setDT(wtd.Ecdf(x, wt))
+    setkey(cdf, x)
+    cdf <- unique(cdf)
+    icdf <- cdf[,splinefun(ecdf, x)]
+    hpd(icdf)
+}
