5b3b3b3fb2e5bdb937188ab0a9784a975b48543d
Rosemary <rmcclosk.math@gmail.com>
2016-06-28 17:16:15 -0700

    working on defence talk

--- doc/figures/objects.R
+++ doc/figures/objects.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
-suppressPackageStartupMessages(library(netabc))
-library(RColorBrewer)
+suppressPackageStartupMessages(library(ape))
+suppressPackageStartupMessages(library(igraph))
+library(png)
 
 n <- 40
 t <- 8
@@ -10,8 +11,20 @@ trans.tree <- ladderize(rcoal(t))
 phylo <- ladderize(rtree(t))
 
 pdf("objects.pdf", width=7, height=2)
-par(mfrow=c(1, 3), mar=c(0, 0, 0, 0))
+par(mfrow=c(1, 4), mar=c(0, 0, 0, 0))
 plot(net, vertex.label=NA, vertex.size=3, vertex.color="black")
 plot(trans.tree, show.tip.label=FALSE, direction="down", edge.width=2)
 plot(phylo, type="unrooted", show.tip.label=FALSE, edge.width=2)
+plot.new()
+y <- seq(0.1, 0.9, length.out=5)
+segments(0.25, y, 1, y, lwd=2)
+
+nmut <- 12
+xmut <- sample(seq(0.25, 1, 0.01), nmut, replace=TRUE)
+ymut <- sample(y, nmut, replace=TRUE)
+segments(xmut, ymut-0.03, xmut, ymut+0.03, lwd=2)
+
+virus <- readPNG("stock/virus.png")
+rasterImage(virus, 0, y-0.08, 0.18, y+0.08)
+
 dev.off()
